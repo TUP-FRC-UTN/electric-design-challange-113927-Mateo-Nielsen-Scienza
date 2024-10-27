865520f97012a5b10e85f426668b2d710717fbd5
Mateo Scienza <mateoscns@gmail.com>
2024-10-27 19:07:04 -0300

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BudgetViewComponent } from "./budget-view/budget-view.component";
+import { BudgetFormComponent } from "./budget-form/budget-form.component";
+import { BudgetListComponent } from './budget-list/budget-list.component';
+import { HttpClientModule } from '@angular/common/http'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BudgetViewComponent, BudgetFormComponent, BudgetListComponent, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'electric-design-challange';
+
+  showForm = false;
+  selectedBudget: number | null = null;
+  budgets = [
+    { id: 1, date: '2024-10-01', client: 'Cliente 1' },
+    { id: 2, date: '2024-10-02', client: 'Cliente 2' },
+    { id: 3, date: '2024-10-03', client: 'Cliente 3' },
+  ];
+
+  onBudgetSelected(budgetId: string) { 
+    console.log('Selected budget ID:', budgetId);
+  }
+  
+
+  viewBudget(budgetId: number) {
+    this.selectedBudget = budgetId;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { BudgetFormComponent } from './budget-form/budget-form.component';
+import { BudgetListComponent } from './budget-list/budget-list.component';
+import { BudgetViewComponent } from './budget-view/budget-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'budgets/new', component: BudgetFormComponent },
+    { path: 'budgets', component: BudgetListComponent },
+    { path: 'budgets/:id', component: BudgetViewComponent },
+    { path: '', redirectTo: '/budgets', pathMatch: 'full' }
+  ];
